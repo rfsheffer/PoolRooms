cca84641829abd2da9c9becbfd486a8fc96f5e36
rfsheffer <rfsheffer@gmail.com>
2024-01-02 20:53:16 -0800

    - Added quick sand trigger stub for adding quicksand and water hazards

---
+++ Assets/Scripts/LethalCompanyTypes/QuicksandTrigger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class QuicksandTrigger : MonoBehaviour
+{
+	public bool isWater;
+
+	public int audioClipIndex;
+
+	public bool sinkingLocalPlayer;
+
+	public float movementHinderance = 1.6f;
+
+	public float sinkingSpeedMultiplier = 0.15f;
+}
