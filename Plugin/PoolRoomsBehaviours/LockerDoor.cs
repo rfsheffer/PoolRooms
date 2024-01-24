5cf96fe60c415b315cc788e0d1b9f7d96b055546
rfsheffer <rfsheffer@gmail.com>
2024-01-23 21:00:30 -0800

    - created doors for the lockers, and started setting up the networked locker door

---
+++ Plugin/PoolRoomsBehaviours/LockerDoor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Unity.Netcode;
+using UnityEngine;
+
+
+namespace PoolRooms
+{
+    public class LockerDoor : NetworkBehaviour
+    {
+        private void Awake()
+        {
+            
+        }
+    }
+}
